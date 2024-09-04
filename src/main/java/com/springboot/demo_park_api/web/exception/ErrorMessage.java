72a593da9224b7847bd586ccce2349ab66a76695
jefersonoliveira <adv.jefersonoliveira@gmail.com>
2024-09-04 14:52:09 -0300

    Upd: Tratando exceção lançada no processo de validação de campos

---
+++ src/main/java/com/springboot/demo_park_api/web/exception/ErrorMessage.java
@@ -0,0 +1,57 @@
+package com.springboot.demo_park_api.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter @ToString
+public class ErrorMessage {
+    //campo para mostar o recurso que gerou a exceção
+    private String path;
+
+    //mostrar qual método http foi gerado a exceção
+    private String method;
+
+    private int status;
+
+    private String statusText;
+
+    private String message;
+
+    private Map<String, String> errors;
+
+    public ErrorMessage() {
+    }
+
+    //criamos um construtor vazio para receber valores como arg. que não foram declarados na classe
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErrors(result);
+    }
+
+    private void addErrors(BindingResult result) {
+        //esse map tera o nome do campo como chave e o valor será a mensagem do erro
+        this.errors = new HashMap<>();
+        for(FieldError fieldError : result.getFieldErrors()) {
+            this.errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
