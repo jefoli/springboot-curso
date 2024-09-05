6e7f1be5c4fb0e29cf9e3f869c27ae304870a6af
jefersonoliveira <adv.jefersonoliveira@gmail.com>
2024-09-05 14:21:53 -0300

    Teste ponta a ponta para criar um novo usuário

--- src/test/java/com/springboot/demo_park_api/UsuarioIT.java
+++ src/test/java/com/springboot/demo_park_api/UsuarioIT.java
@@ -1,12 +1,16 @@
 package com.springboot.demo_park_api;
 
+import com.springboot.demo_park_api.web.dto.UsuarioCreateDto;
+import com.springboot.demo_park_api.web.dto.UsuarioResponseDto;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "/sql/usuarios/usuarios-inset.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 
 public class UsuarioIT {
@@ -14,4 +18,29 @@ public class UsuarioIT {
     @Autowired
     WebTestClient testClient;
 
+    //testar novo usuario
+    @Test
+    public void createUsuario_ComUsernameEPasswordValidos_RetornarUsuarioCriadoComStatus201(){
+        //var testeCliente oferece uma lista de opções de testes
+
+        UsuarioResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("tody@email.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+
+        //verificar o obj retornado pela requisição
+        //essa classe dá acesso aos métodos que iremos testar os objetos
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+
+
+
+    }
 }
