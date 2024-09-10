0f80e6e8cf6d801b4e499afa789f4e052a5fbcbd
jefersonoliveira <adv.jefersonoliveira@gmail.com>
2024-09-10 14:10:53 -0300

    Upd: Classe de Autenticação controller.

---
+++ src/main/java/com/springboot/demo_park_api/web/controller/AutenticacaoController.java
@@ -0,0 +1,47 @@
+package com.springboot.demo_park_api.web.controller;
+
+import com.springboot.demo_park_api.jwt.JwtToken;
+import com.springboot.demo_park_api.jwt.JwtUserDetailsService;
+import com.springboot.demo_park_api.web.dto.UsuarioLoginDto;
+import com.springboot.demo_park_api.web.exception.ErrorMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class AutenticacaoController {
+    private final JwtUserDetailsService deatailsService;
+    private final AuthenticationManager authenticationManager;
+
+    //met recurso de autenticacao
+    @PostMapping("/auth")
+    public ResponseEntity<?> autenticar(@RequestBody @Valid UsuarioLoginDto dto, HttpServletRequest request) {
+        log.info("Processo de autenticação pelo login {}", dto.getUsername());
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword());
+            authenticationManager.authenticate(authenticationToken);
+            JwtToken token = deatailsService.getTokenAuthenticated(dto.getUsername());
+            return ResponseEntity.ok(token);
+
+        } catch (AuthenticationException ex) {
+            log.warn("Bad Credentials from username '{}", dto.getUsername());
+        }
+
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Credenciais inválidas"));
+    }
+}
