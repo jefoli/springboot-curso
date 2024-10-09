e4989e1f4301e2b6859634a6910f2d2fb0e711c6
jefersonoliveira <adv.jefersonoliveira@gmail.com>
2024-10-09 18:44:04 -0300

    Upd: Documentação e Teste do recurso para buscar dados do cliente

--- src/main/java/com/springboot/demo_park_api/web/controller/ClienteController.java
+++ src/main/java/com/springboot/demo_park_api/web/controller/ClienteController.java
@@ -123,6 +123,24 @@ public class ClienteController {
         return ResponseEntity.ok(PageableMapper.toDto(clientes));
     }
 
+
+    @Operation(summary = "Recuperar dados do cliente autenticado",
+            description = "Requisição exige uso de um bearer token. Acesso restrito a Role='CLIENTE'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso",
+                        content = @Content(mediaType = " application/json;charset=UTF-8",
+                                schema = @Schema(implementation = ClienteResponseDto.class)
+                        )
+                ),
+                @ApiResponse(responseCode = "403", description = "REcurso não permitido ao perfil de ADMIN",
+                        content = @Content(mediaType = " application/json;charset=UTF-8",
+                                schema = @Schema(implementation = ClienteResponseDto.class)
+                        )
+                )
+
+            }
+    )
     @GetMapping("/detalhes")
     @PreAuthorize("hasRole('CLIENTE')")
     public ResponseEntity<ClienteResponseDto> getDetalhes(@AuthenticationPrincipal JwtUserDetails userDetails) {
