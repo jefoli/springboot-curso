c1d4bbdf97d093146e5c0a4a817a73d8ee87dcbb
jefersonoliveira <adv.jefersonoliveira@gmail.com>
2024-10-13 11:09:36 -0300

    Upd: Documentação e Teste do recurso para inserir uma vaga com status 200

--- src/main/java/com/springboot/demo_park_api/web/controller/VagaController.java
+++ src/main/java/com/springboot/demo_park_api/web/controller/VagaController.java
@@ -5,8 +5,17 @@ import com.springboot.demo_park_api.service.VagaService;
 import com.springboot.demo_park_api.web.dto.VagaCreateDto;
 import com.springboot.demo_park_api.web.dto.VagaResponseDto;
 import com.springboot.demo_park_api.web.dto.mapper.VagaMapper;
+import com.springboot.demo_park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +23,29 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@Tag(name = "Vagas", description = "Contém todas as operações relativas ao recurso de uma vaga")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/vagas")
 public class VagaController {
 
     private final VagaService vagaService;
+
+    @Operation(summary = "Criar uma nova vaga", description = "Recurso para criar uma nova vaga." +
+        "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
+        responses = {
+            @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                headers = @Header(name = HttpHeaders.LOCATION, description = "URL do recurso criado")),
+
+            @ApiResponse(responseCode = "409", description = "Vaga já cadastrada",
+                content = @Content(mediaType = " application/json;charset=UTF-8",
+                    schema = @Schema(implementation = ErrorMessage.class))),
+
+            @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                    content = @Content(mediaType = " application/json;charset=UTF-8",
+                    schema = @Schema(implementation = ErrorMessage.class)))
+        }
+    )
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
@@ -37,6 +63,18 @@ public class VagaController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(summary = "Localizar uma vaga", description = "Recurso para retornar uma vaga pelo código" +
+        "Requisição exige uso de um berar token. Acesso restrito a Role='ADMIN'",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Recurso criado com sucesso",
+                content = @Content(mediaType = " application/json;charset=UTF-8",
+                    schema = @Schema(implementation = VagaResponseDto.class))),
+
+            @ApiResponse(responseCode = "404", description = "Vaga não localizada",
+                content = @Content(mediaType = " application/json;charset=UTF-8",
+                    schema = @Schema(implementation = ErrorMessage.class)))
+        }
+    )
     //metodo para acessar vaga
     @GetMapping("/{codigo}")
     @PreAuthorize("hasRole('ADMIN')")
