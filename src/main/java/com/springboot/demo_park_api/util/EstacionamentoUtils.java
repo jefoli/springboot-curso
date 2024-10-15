d844e3b0d5035b2dec756641d895bab900a681fa
jefersonoliveira <adv.jefersonoliveira@gmail.com>
2024-10-15 14:25:38 -0300

    Upd:  Método para geração do número do recibo de estacionamento

---
+++ src/main/java/com/springboot/demo_park_api/util/EstacionamentoUtils.java
@@ -0,0 +1,25 @@
+package com.springboot.demo_park_api.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EstacionamentoUtils {
+
+    //metodo - hora de entrada + gerar num de recibo
+    public static String gerarRecibo() {
+
+        LocalDateTime date = LocalDateTime.now();
+
+        String recibo = date.toString().substring(0,19);
+
+        return recibo
+                .replace("-","")
+                .replace(":", "")
+                .replace("T", "-");
+
+    }
+
+}
